427e57765b5cf3f912976fe31c65b0a04bf0a229
katanydarmawan <rizky.darmawan1818@gmail.com>
2020-10-03 14:11:45 +0700

    menambahkan paragraf

--- index.php
+++ index.php
@@ -1,11 +1,15 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Gi</title>
 </head>
+
 <body>
     <h1>Hellow World</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima doloribus natus tempore omnis fugit totam dicta odio expedita? Minima facere odit corporis dolor, fugiat dicta. Quae sed obcaecati nisi aperiam.</p>
 </body>
+
 </html>
