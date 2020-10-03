499d78190538ddf65ee2facd6fb9196f44b9e416
katanydarmawan <rizky.darmawan1818@gmail.com>
2020-10-03 14:17:16 +0700

    menabahkan list

--- index.php
+++ index.php
@@ -10,6 +10,10 @@
 <body>
     <h1>Hellow World</h1>
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima doloribus natus tempore omnis fugit totam dicta odio expedita? Minima facere odit corporis dolor, fugiat dicta. Quae sed obcaecati nisi aperiam.</p>
+    <ul>
+        <li>satu</li>
+        <li>satu</li>
+    </ul>
 </body>
 
 </html>
